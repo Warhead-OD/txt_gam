9a3b9236aa12ef6f388d59dc038953c122138403
Warhead-OD <SimeonK2007@gmail.com>
2023-02-09 13:09:37 -0700

    Imported RPS files

---
+++ Main.py
@@ -0,0 +1,50 @@
+from random import randint
+
+# This RPS code is something that I have been working on for a while.
+# It IS copy and pasted from another one of my repos because I didn't
+# want to have to retype it or deal with the importing.
+# There are some modifications for the purposes of this program
+def rps():
+	# this determines the computer's choice
+	options = ["Rock", "Paper", "Scissors"]
+	options_short = ["R", "P", "S"]
+	c_choice_l = options[randint(0, 2)]
+	# this will determine the user's choice
+	u_choice = input("Rock, Paper, or Scissors?\n")
+	
+	# this determines and displays the winner
+	try:
+		c_choice = c_choice_l[0].upper()
+		u_choice = u_choice[0].upper()
+		options_short.index(u_choice)
+		print(f"\nThe computer chose {c_choice_l}!")
+		if u_choice == c_choice:
+			input("It was a tie!")
+		elif u_choice == "R":
+			if c_choice == "S":
+				input("You won!")
+			else:
+				input("You lost!")
+		elif u_choice == "P":
+			if c_choice == "R":
+				input("You won!")
+			else:
+				input("You lost!")
+		elif u_choice == "S":
+			if c_choice == "P":
+				input("You won!")
+			else:
+				input("You lost!")
+	# this will run if the user gives an invalid response
+	except ValueError:
+		print("Invalid choice!")
+		rps()
+
+
+# This is the code for running the program
+def main():
+	print("PH")
+
+
+if __name__ == "__main__":
+	main()
