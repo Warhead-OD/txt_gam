613c544e34ff4723cdf963f6f7ce585aed557cd0
Warhead-OD <SimeonK2007@gmail.com>
2023-02-14 13:19:18 -0700

    Home Room

--- Main.py
+++ Main.py
@@ -1,5 +1,23 @@
 from random import randint
 
+
+# This is the function that will be used for calling the help function
+# and/or displaying more detailed information on that command
+def help():
+	print("\n The commands are;")
+	print("north, east, south, west")
+	temp = input("Type one of these commands to learn more about it, otherwise, just press ENTER\n")
+	if temp == "north":
+		print("Used to move the character up by one room.")
+	elif temp == "east":
+		print("Used to move the character to the right by one room.")
+	elif temp == "south":
+		print("Used to move the character down by one room.")
+	elif temp == "west":
+		print("Used to move the character to the left by one room.")
+	input()
+	print()
+
 # This RPS code is something that I have been working on for a while.
 # It IS copy and pasted from another one of my repos because I didn't
 # want to have to retype it or deal with the importing.
@@ -47,18 +65,34 @@ def rps():
 
 # This is the code for running the program
 def main():
+	print()
 	# Formatted as (visited, story progression)
 	# Starting Screen, 
-	world_location_data = ((0, 0), (0, 0))
+	world_location_data = ([0, 0], [0, 0])
+	cur_location = 0
 	#
 	world_items = ()
 	
 	# This is a set list of the allowed inputs from the user for
 	# each of their turns. It is auto formatted as a .lower()
 	allowed_inputs = ("north", "east", "south", "west")
-	
-	while True:
-		print(rps())
+
+	# This set of if statements are going to be used to figure out
+	# where the player currently is and then will display some text
+	# statement based on what they have already done in the game
+		# This is the code for the starting room
+	if cur_location == 0:
+		v = world_location_data[0][0]
+		sp = world_location_data[0][1]
+		if not v:
+			print("Welcome to my humble Text Game!\n")
+			if input("Would you like the list of commands to the game? yes/no\n") == "yes":
+				help()
+			print("\nAs you join into this strange new digital world, you feel as if you are being watched.")
+			world_location_data[0][0] = 1
+		print("All around you are grand white pillars that seem to stretch high into the sky. The sky")
+		###################################################################################################################################################
+		print("There are exits to this room to the:\n  north")
 
 
 if __name__ == "__main__":
